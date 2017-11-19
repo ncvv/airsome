37e41f67e14ca6ddb61755ec029c57dc5e2e16fd
Kerstin <kerstin@github.com>
2017-11-19 17:11:48 +0100

    Nothing

--- src/classifier.py
+++ src/classifier.py
@@ -9,8 +9,8 @@ def naive_bayes(dataset):
     from sklearn.metrics import accuracy_score
 
     encoder = OrdinalEncoder()
-    dataset_encoded = encoder.fit_transform(dataset[['neighbourhood_cleansed','is_location_exact','property_type', 'room_type', 'bed_type', 'price','cancellation_policy','host_response_rate_binned','perceived_quality']])
-    listings_data=dataset_encoded[['neighbourhood_cleansed','is_location_exact','property_type', 'room_type', 'bed_type', 'price','cancellation_policy','host_response_rate_binned']]
+    dataset_encoded = encoder.fit_transform(dataset)
+    listings_data=dataset_encoded.drop(columns=['id','perceived_quality'])
     listings_target= dataset_encoded['perceived_quality']
     
     data_train, data_test, target_train, target_test = train_test_split(listings_data, listings_target, test_size=0.2, random_state=42, stratify=listings_target)
@@ -19,10 +19,10 @@ def naive_bayes(dataset):
     naive_bayes.fit(data_train, target_train)
     prediction = naive_bayes.predict(data_test)
 
-    nbresults=pd.DataFrame(data_test)
-    quality_predicted = nbresults.assign(predicted_quality=prediction)
-
+    #nbresults=pd.DataFrame(data_test)
+    #quality_predicted = nbresults.assign(predicted_quality=prediction)
     #io.write_csv(quality_predicted, '../data/playground/naivebayes.csv')
+    
     accuracy=accuracy_score(target_test, prediction)
     print('Accuracy of Naive Bayes Classifier:{}'.format(accuracy))
 
