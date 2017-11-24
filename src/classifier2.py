9e491b5df2711b0d6938e1baa3e74e11344e3b5a
nseemann <nseemann@mail.uni-mannheim.de>
2017-11-24 23:50:37 +0100

    TODOS classifier2.py - will talk in skype session about it

--- src/classifier2.py
+++ src/classifier2.py
@@ -42,11 +42,11 @@ class Classifier(object):
     def encode_and_split(self):
         ''' Encode datatset and split it into training and test data. '''
         
-        - #TODO muessen wir das dann anpassen ? 
+        - #TODO muessen/ wollen wir das dann anpassen ? 
         encoder = OrdinalEncoder()
-       
         self.dataset_encoded = encoder.fit_transform(self.dataset[['experiences_offered', 'host_location', 'host_response_time', 'host_is_superhost', 'neighbourhood_cleansed', 'review_scores_value', 'instant_bookable', 'cancellation_policy',
                                                                    'require_guest_profile_picture', 'require_guest_phone_verification', 'calculated_host_listings_count', 'reviews_per_month', 'host_response_rate_binned', 'host_verification_binned']])
+        
         self.data_train, self.data_test, self.target_train, self.target_test = train_test_split(self.dataset_encoded, self.dataset['perceived_quality'], test_size=0.2, random_state=42, stratify=self.dataset['perceived_quality'])
 
     def classify_nb(self): 
@@ -80,10 +80,11 @@ class Classifier(object):
             self.accuracy_nc = acc
     def svm(self ): #, C=1.0, gamma ='auto'): # TODO muss ich hier dann nicht auch die paras übergeben?
         ''' Classification with Support Vector Machine. '''
-
         svc = SVC()
         svc.fit(self.dataset_encoded, self.dataset['perceived_quality'])
+        #svc.fit(data_train, target_train)
         prediction = svc.predict(self.data_test)
+         #TODO hier wird bei mir ein FEHLER geworfen muss ein [[]] sein statt []
         self.accuracy_svm = svc.score(self.target_test, prediction)
         acc = svc.score(self.target_test, prediction)
         if acc > self.accuracy_svm:
