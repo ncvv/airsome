29db7c58d780eea84d0efee40089f50c654b7757
Nicolas Wipfler <ncvv@users.noreply.github.com>
2017-11-27 16:25:02 +0100

    Adjust process call according to method changes.

--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ import classifier as cl
 
 def main(renew_listings=False):
     ''' Main method. '''
+    long_tfidf = True
+    num_labels = 2
+    
     if renew_listings:
         listings = io.read_csv('../data/original/listings.csv')
         pp.Preprocessor.prepare_listings_data(listings)
@@ -18,20 +21,13 @@ def main(renew_listings=False):
         listings = io.read_csv('../data/processed/listings_processed.csv')
         listings_text = io.read_csv('../data/processed/listings_text_processed.csv')
         preprocessor = pp.Preprocessor(False, requests.Session(), listings, listings_text, reviews)
-        preprocessor.process()
+        preprocessor.process(num_labels, long_tfidf)
 
     # Classification
-    file_name = 'dataset'
-    
-    long_tfidf = True
-    num_labels = 2
+    file_name = 'dataset_' + str(num_labels) + '_long_tfidf' if long_tfidf else ''
 
-    file_name += '_' + str(num_labels)
-
-    if long_tfidf:
-        file_name += '_long_tfidf'
     dataset = io.read_csv('../data/final/' + file_name + '.csv')
-    
+
     encoded_file_path = '../data/final/' + file_name + '_encoded.csv'
     
     print('#Columns: ' + str(len(list(dataset))))
